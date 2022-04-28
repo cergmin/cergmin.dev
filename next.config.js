1e06736bf9c411506fd85d4f1049b6584c82e10a
Sergey Minakov <cergmin@gmail.com>
2022-04-28 21:18:46 +1000

    Moved all the output to the /build folder

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 const { join } = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+const outputFolder = 'build';
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  distDir: `${outputFolder}/.next`,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(
       new CopyPlugin({
         patterns: [
           {
             from: join(__dirname, 'content'),
-            to: '../.data/content',
+            to: join(__dirname, `${outputFolder}/.data/content`),
           },
         ],
       }),

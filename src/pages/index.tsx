62a364718ba0728818b2d0a7e46e933b79a728be
Sergey Minakov <cergmin@gmail.com>
2022-05-10 18:45:27 +1000

    Add home page with introduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,18 @@ function HomePage() {
     <main>
       <div className="wrapper">
         <div className={s.layout}>
-          <h1 className="pageTitle">Главная</h1>
+          <h1 className={s.introductionTitle}>
+            Привет, меня&nbsp;зовут
+            <br />
+            <span className={s.name}>Минаков Сергей.</span>
+          </h1>
+          <p className={s.introductionDescription}>
+            Я фронтенд разработчик!
+            <br />
+            Пишу приложения на JavaScript,
+            <br />
+            обычно использую React и Next.js.
+          </p>
         </div>
       </div>
     </main>
